8bc022ff25690c19e76ae62ef9ff53590d3850f5
Ryan Wold <wold@afomi.com>
2015-06-25 21:31:45 -0700

    change Scene to Component. use an .html file as `tree`
    http://famous.org/framework/AppleTV/behaviors.html

--- components/afomi/apple-tv/apple-tv.js
+++ components/afomi/apple-tv/apple-tv.js
@@ -1,13 +1,33 @@
-FamousFramework.scene('afomi:apple-tv', {
-    behaviors: {
-        '#surface': {
-            'content': '<h1>Hello afomi:apple-tv!</h1>',
-            'size': [200, 200],
-            'style': {
-                'background-color': 'red'
-            }
-        }
+FamousFramework.component('afomi:apple-tv', {
+  behaviors: {
+    '#root': {
+      'style': {
+        'background-color': 'grey',
+        'perspective': '1000px'
+      }
     },
-
-    tree: '<node id="surface"></node>'
+    '#rotator-node': {
+      'size': [500,500],
+      'align': [0.5, 0.5],
+      'mount-point': [0.5, 0.5],
+      'style': {
+        'background-color': 'red'
+      }
+    },
+    '.gallery-item': {
+      'size': [100, 100],
+      'style': {
+        'background-color': 'blue'
+      },
+      '$repeat': function () {
+        return [1, 2, 3, 4];
+      },
+      'position': function($index) {
+        return [Math.random() * 500, Math.random() * 500];
+      }
+    }
+  },
+  events: {},
+  states: {},
+  tree: 'apple-tv.html'
 });
