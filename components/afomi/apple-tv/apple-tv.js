87db8576423f8d96c3de477828530662324f6fa2
Ryan Wold <wold@afomi.com>
2015-06-25 21:54:29 -0700

    use `states` from `behaviors`. reposition rotator node
    http://famous.org/framework/AppleTV/adding-state.html

--- components/afomi/apple-tv/apple-tv.js
+++ components/afomi/apple-tv/apple-tv.js
@@ -2,32 +2,50 @@ FamousFramework.component('afomi:apple-tv', {
   behaviors: {
     '#root': {
       'style': {
-        'background-color': 'grey',
         'perspective': '1000px'
       }
     },
     '#rotator-node': {
-      'size': [500,500],
+      'size': function(contextSize) {
+        return [contextSize, contextSize];
+      },
       'align': [0.5, 0.5],
       'mount-point': [0.5, 0.5],
+      'origin': [0.5, 0.5],
       'style': {
         'background-color': 'red'
+      },
+      'rotation': function(rotationValue) {
+        return [-Math.PI/2.1, 0, rotationValue]
       }
     },
     '.gallery-item': {
       'size': [100, 100],
       'style': {
-        'background-color': 'blue'
+        'background-color': 'blue',
+        'border': '2px solid black'
       },
-      '$repeat': function () {
-        return [1, 2, 3, 4];
+      '$repeat': function (srcs) {
+        return srcs;
       },
-      'position': function($index) {
-        return [Math.random() * 500, Math.random() * 500];
-      }
+      'position-x': function($index, contextSize) {
+        return Math.random() * contextSize;
+      },
+      'position-y': function($index, contextSize) {
+        return Math.random() * contextSize;
+      },
+      'position-z': function($index, positionZ) {
+        return positionZ[$index];
+      },
+      'rotation': [Math.PI/2, 0, 0]
     }
   },
   events: {},
-  states: {},
+  states: {
+    rotationValue: 0,
+    srcs: [1, 2, 3, 4],
+    contextSize: 500,
+    positionZ: []
+  },
   tree: 'apple-tv.html'
 });
